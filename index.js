02e320e90a266e1468f66af642dc9d81826809a2
Adam Cox <adamcox.dev@gmail.com>
2022-09-21 11:36:05 -0600

    First commit on demo

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const path = require('path');
+
+const port = process.env.PORT || 5000
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log('Running on ' + port)
+})
